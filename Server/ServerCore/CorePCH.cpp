dc75762766b565f28b6f22529d14092029543781
9kyo-hwang <ryghkd507@naver.com>
2025-06-21 19:36:08 +0900

    [Feature] 환경 설정

---
+++ Server/ServerCore/CorePCH.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "CorePCH.h"
+
+void Test()
+{
+	std::cout << "Hello, Server!\n";
+}
---
+++ Server/ServerCore/CorePCH.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Types.h"
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+void Test();
