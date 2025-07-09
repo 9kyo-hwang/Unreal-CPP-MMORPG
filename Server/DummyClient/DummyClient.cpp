07153c627c6d7d6091364803504aea49e5318b6f
9kyo-hwang <ryghkd507@naver.com>
2025-07-09 16:03:37 +0900

    [Feature] 소켓 프로그래밍 기초

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,6 +1,64 @@
 ﻿#include "pch.h"
 
+#include <WinSock2.h>	// for Socket Programming
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")	// 반드시 필요
+
 int main()
 {
+	uint16 VersionRequested = MAKEWORD(2, 2);
+	WSADATA OutData;
+	if (::WSAStartup(VersionRequested, /*거의 사용할 일 없음*/&OutData) != 0)
+	{
+		cout << "[WSAStartup] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
 
+	uint16 AddressFamily = AF_INET;	// IPv4
+	int32 Type = SOCK_STREAM;	// AF_INET + TCP
+	int32 Protocol = 0;	// 내부적으로 알아서 프로토콜을 세팅해줌. 우리의 경우 TCP가 선택됨
+	uint64 Client = ::socket(AddressFamily, Type, Protocol);	// 일종의 번호(Descriptor)
+	if (Client == INVALID_SOCKET)
+	{
+		cout << "[socket] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	uint16 HostPort = 7777;
+	SOCKADDR_IN ServerAddress
+	{
+		.sin_family = AddressFamily,
+		.sin_port = ::htons(HostPort),
+		.sin_addr = {},
+		.sin_zero = {},
+	};
+
+	const char* AddressString = "127.0.0.1";	// 일단 내 컴퓨터
+	IN_ADDR* AddressBuffer = &ServerAddress.sin_addr;
+	if (::inet_pton(AddressFamily, AddressString, AddressBuffer) != 1)
+	{
+		cout << "[inet_pton] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	if (::connect(Client, reinterpret_cast<SOCKADDR*>(&ServerAddress), sizeof(ServerAddress)) == SOCKET_ERROR)
+	{
+		cout << "[connect] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// TODO
+
+		this_thread::sleep_for(1s);
+	}
+
+	::closesocket(Client);
+	::WSACleanup();
+
+	return 0;
 }
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -8,72 +8,81 @@
 #include "RefCountBase.h"
 #include "ThreadManager.h"
 
-using ActorList = TypeList<class Player, class Knight, class Monster>;
-
-class Player
-{
-public:
-	Player()
-	{
-		TypeId = IndexOf<Types, Player>::Value;
-	}
-
-	virtual ~Player() {}
-
-	using Types = ActorList;
-	int32 TypeId;
-
-private:
-};
-
-class Knight : public Player
-{
-public:
-	Knight()
-	{
-		TypeId = IndexOf<Types, Knight>::Value;
-	}
-	~Knight() override {}
-
-	int32 Hp = rand() % 1000;
-};
-
-class Monster
-{
-public:
-	Monster()
-	{
-		TypeId = IndexOf<Types, Monster>::Value;
-	}
-
-	~Monster() {}
-
-	using Types = ActorList;
-	int32 TypeId;
-};
+#include <WinSock2.h>	// for Socket Programming
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	//TypeList<Knight>::First SingleHead;
-	//TypeList<Knight, Monster>::First PairHead;
-	//TypeList<Knight, Monster>::Second PairTail;
-	//TypeList<Knight, TypeList<Monster, Monster>>::First DPHead;
-	//TypeList<Knight, TypeList<Monster, Monster>>::Second::First DPTailHead;
-	//TypeList<Knight, TypeList<Monster, Monster>>::Second::First DPTailTail;
-
-	//int64 Len = SizeOf<TypeList<Knight, Monster, Monster>>::Value;
-	//TypeAt<TypeList<Knight, Monster, Knight>, 2>::Value Type2;
-	//int64 Index = IndexOf<TypeList<Knight, Monster>, Knight>::Value;
-
-	//bool canCastFromPlayerToKnight = Convert<Player, Knight>::Result;
-	//bool canCastFromKnightToPlayer = Convert<Knight, Player>::Result;
-	//bool canCastFromPlayerToMonster = Convert<Player, Monster>::Result;
-
+	uint16 VersionRequested = MAKEWORD(2, 2);
+	WSADATA OutData;
+	if (::WSAStartup(VersionRequested, /*거의 사용할 일 없음*/&OutData) != 0)
 	{
-		shared_ptr<Knight> K = MakeShared<Knight>();
-		shared_ptr<Player> P = Cast<Player>(K);
-		bool CanCast = IsA<Player>(K);
+		cout << "[WSAStartup] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
 	}
+
+	uint16 AddressFamily = AF_INET;	// IPv4
+	int32 Type = SOCK_STREAM;	// AF_INET + TCP
+	int32 Protocol = 0;	// 내부적으로 알아서 프로토콜을 세팅해줌. 우리의 경우 TCP가 선택됨
+	uint64 Listen = ::socket(AddressFamily, Type, Protocol);	// 일종의 번호(Descriptor)
+	if (Listen == INVALID_SOCKET)
+	{
+		cout << "[socket] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	uint16 HostPort = 7777;
+	SOCKADDR_IN MyAddress	// 서버 입장에서는 내 주소
+	{
+		.sin_family = AddressFamily,
+		.sin_port = ::htons(HostPort),
+		.sin_addr = {},
+		.sin_zero = {},
+	};
+
+	IN_ADDR& AddressBuffer = MyAddress.sin_addr;
+	AddressBuffer.s_addr = ::htonl(INADDR_ANY);	// 고정된 주소가 아닌, 적절한 모든 주소에 연결됨
+
+	if (::bind(Listen, reinterpret_cast<SOCKADDR*>(&MyAddress), sizeof(MyAddress)) == SOCKET_ERROR)
+	{
+		cout << "[bind] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	int32 Backlog = 10;
+	if (::listen(Listen, Backlog) == SOCKET_ERROR)
+	{
+		cout << "[listen] function failed with error: " << ::WSAGetLastError() << endl;
+		return 1;
+	}
+
+	while (true)
+	{
+		SOCKADDR_IN ClientAddress{};
+		int32 AddressLength = sizeof(ClientAddress);
+		uint64 Client = ::accept(Listen, reinterpret_cast<SOCKADDR*>(&ClientAddress), &AddressLength);
+		if (Client == INVALID_SOCKET)
+		{
+			cout << "[accept] function failed with error: " << ::WSAGetLastError() << endl;
+			break;
+		}
+
+		const uint16 StringBufferSize = INET_ADDRSTRLEN;
+		char StringBuffer[StringBufferSize]{};
+		if (::inet_ntop(AddressFamily, &ClientAddress.sin_addr, StringBuffer, StringBufferSize) == nullptr)
+		{
+			cout << "[inet_ntop] function failed with error: " << ::WSAGetLastError() << endl;
+			break;
+		}
+
+		cout << "Client Connected! IP = "<< StringBuffer << endl;
+
+		// TODO
+	}
+
+	::WSACleanup();
 
 	return 0;
 }
