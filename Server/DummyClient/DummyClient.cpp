f7dd2555fd5bb03406f4531c6cc91c4de1187697
9kyo-hwang <ryghkd507@naver.com>
2025-07-14 19:27:12 +0900

    [Feature] 논블로킹 소켓

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -3,11 +3,41 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")	// 반드시 필요
 
-void HandleError(SOCKET SocketToClose, const char* Ftn)
+void HandleError(const char* Ftn)
 {
 	printf("Error at %s(): %ld\n", Ftn, ::WSAGetLastError());
-	//::closesocket(SocketToClose);
-	//::WSACleanup();
+}
+
+int TryReceive(const SOCKET& ClientSocket, char* ReceiveBuffer, int32& OutReceiveLength)
+{
+	OutReceiveLength = ::recv(ClientSocket, ReceiveBuffer, sizeof(ReceiveBuffer), 0);
+
+	if (OutReceiveLength == SOCKET_ERROR)
+	{
+		return ::WSAGetLastError() == WSAEWOULDBLOCK ? 0 : -1;
+	}
+	else if (OutReceiveLength == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int TrySend(const SOCKET& ClientSocket, const char* SendBuffer, int32& OutSendLength)
+{
+	OutSendLength = ::send(ClientSocket, SendBuffer, sizeof(SendBuffer), 0);
+
+	if (OutSendLength == INVALID_SOCKET)
+	{
+		return ::WSAGetLastError() == WSAEWOULDBLOCK ? 0 : -1;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 int main()
@@ -21,11 +51,79 @@ int main()
 		return 1;
 	}
 
-	SOCKET ClientSocket = ::socket(AF_INET, SOCK_DGRAM, 0);	// 일종의 번호(Descriptor)
+	SOCKET ClientSocket = ::socket(AF_INET, SOCK_STREAM, 0);	// 일종의 번호(Descriptor)
 	if (ClientSocket == INVALID_SOCKET)
 	{
-		HandleError(ClientSocket, "socket");
 		return 1;
+	}
+
+	u_long ArgP = 1;
+	if (::ioctlsocket(ClientSocket, FIONBIO, &ArgP) == INVALID_SOCKET)
+	{
+		return 1;
+	}
+
+	SOCKADDR_IN ServerAddress(AF_INET, ::htons(7777));
+	::inet_pton(AF_INET, "127.0.0.1", &ServerAddress.sin_addr);
+
+	while (true)
+	{
+		if (::connect(ClientSocket, reinterpret_cast<SOCKADDR*>(&ServerAddress), sizeof(ServerAddress)) == SOCKET_ERROR)
+		{
+			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			{
+				// 연결 시도 중
+				continue;
+			}
+
+			if (::WSAGetLastError() == WSAEISCONN)
+			{
+				// 앞서 Would Block에 의해 빠꾸먹고 다시 들어온 경우. 이미 연결된 상태라 connect 그만
+				break;
+			}
+
+			// Error
+			break;
+		}
+	}
+
+	cout << "Connected to server!" << endl;
+	char SendBuffer[100] = "Hello, Server!";
+
+	while (true)
+	{
+		int32 SendLen;
+		int32 SendResult = TrySend(ClientSocket, SendBuffer, SendLen);
+		if (SendResult == -1)	// Error
+		{
+			break;
+		}
+		else if (SendResult == 0)	// Would Block
+		{
+			continue;
+		}
+
+		cout << "Send Data! Len = " << SendLen << endl;
+
+		while (true)
+		{
+			char ReceiveBuffer[1000];
+			int32 ReceiveLength;
+			int32 ReceiveResult = TryReceive(ClientSocket, ReceiveBuffer, ReceiveLength);
+			if (ReceiveResult == -1)	// Error or Disconnected
+			{
+				break;
+			}
+			else if (ReceiveResult == 0)	// WouldBlock
+			{
+				continue;
+			}
+
+			cout << "Recv Data Len = " << ReceiveLength << endl;
+			break;
+		}
+
+		this_thread::sleep_for(1s);
 	}
 
 	::closesocket(ClientSocket);
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -3,11 +3,57 @@
 #include <WS2tcpip.h>
 #pragma comment(lib, "ws2_32.lib")
 
-void HandleError(SOCKET SocketToClose, const char* Ftn)
+void HandleError(const char* Ftn)
 {
 	printf("Error at %s(): %ld\n", Ftn, ::WSAGetLastError());
-	//::closesocket(SocketToClose);
-	//::WSACleanup();
+}
+
+int TrySend(const SOCKET& ClientSocket, const char* SendBuffer, int32& OutSendLength)
+{
+	OutSendLength = ::send(ClientSocket, SendBuffer, sizeof(SendBuffer), 0);
+
+	if (OutSendLength == INVALID_SOCKET)
+	{
+		return ::WSAGetLastError() == WSAEWOULDBLOCK ? 0 : -1;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int TryReceive(const SOCKET& ClientSocket, char* ReceiveBuffer, int32& OutReceiveLength)
+{
+	OutReceiveLength = ::recv(ClientSocket, ReceiveBuffer, sizeof(ReceiveBuffer), 0);
+
+	if (OutReceiveLength == SOCKET_ERROR)
+	{
+		return ::WSAGetLastError() == WSAEWOULDBLOCK ? 0 : -1;
+	}
+	else if (OutReceiveLength == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int TryAccept(const SOCKET& ListenSocket, SOCKET& OutClientSocket)
+{
+	SOCKADDR_IN ClientAddress;
+	int32 AddressLength = sizeof(ClientAddress);
+	OutClientSocket = ::accept(ListenSocket, reinterpret_cast<SOCKADDR*>(&ClientAddress), &AddressLength);
+
+	if (OutClientSocket == INVALID_SOCKET)
+	{
+		return ::WSAGetLastError() == WSAEWOULDBLOCK ? 0 : -1;
+	}
+	else
+	{
+		return 1;
+	}
 }
 
 int main()
@@ -21,23 +67,80 @@ int main()
 		return 1;
 	}
 
-	// UDP에서는 Listen Socket이 필요 없음
-	SOCKET ServerSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-	if (ServerSocket == INVALID_SOCKET)
+	SOCKET ListenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (ListenSocket == INVALID_SOCKET)
 	{
-		HandleError(ServerSocket, "socket");
 		return 1;
 	}
 
-	int32 SendBufferSize;
-	int32 SndbufOptLen = sizeof(SendBufferSize);
-	::getsockopt(ServerSocket, SOL_SOCKET, SO_SNDBUF, reinterpret_cast<char*>(&SendBufferSize), &SndbufOptLen);
-	cout << "Send Buffer Size: " << SendBufferSize << endl;
+	u_long ArgP = 1;
+	if (::ioctlsocket(ListenSocket, FIONBIO, &ArgP) == INVALID_SOCKET)
+	{
+		return 1;
+	}
 
-	int32 RecvBufferSize;
-	int32 RcvbufOptLen = sizeof(RecvBufferSize);
-	::getsockopt(ServerSocket, SOL_SOCKET, SO_RCVBUF, reinterpret_cast<char*>(&RecvBufferSize), &RcvbufOptLen);
-	cout << "Receive Buffer Size: " << RecvBufferSize << endl;
+	SOCKADDR_IN ServerAddress(AF_INET, ::htons(7777));
+	ServerAddress.sin_addr.s_addr = ::htonl(INADDR_ANY);
+
+	if (::bind(ListenSocket, reinterpret_cast<SOCKADDR*>(&ServerAddress), sizeof(ServerAddress)) == SOCKET_ERROR)
+	{
+		return 1;
+	}
+
+	if (::listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		return 1;
+	}
+
+	cout << "Accept!" << endl;
+
+	while (true)
+	{
+		SOCKET ClientSocket;
+		int32 AcceptResult = TryAccept(ListenSocket, ClientSocket);
+		if (AcceptResult == -1)
+		{
+			break;
+		}
+		else if (AcceptResult == 0)
+		{
+			continue;
+		}
+
+		cout << "Client connected!" << endl;
+
+		while (true)
+		{
+			char ReceiveBuffer[1000];
+			int32 ReceiveLength;
+			int32 ReceiveResult = TryReceive(ClientSocket, ReceiveBuffer, ReceiveLength);
+			if (ReceiveResult == -1)	// Error or Disconnected
+			{
+				break;
+			}
+			else if (ReceiveResult == 0)	// WouldBlock
+			{
+				continue;
+			}
+
+			cout << "Recv Data Len = " << ReceiveLength << endl;
+
+			while (true)
+			{
+				int32 SendLen;
+				int32 SendResult = TrySend(ClientSocket, ReceiveBuffer, SendLen);
+				if (SendResult == -1)	// Error
+				{
+					break;
+				}
+				else if (SendResult == 1)
+				{
+					cout << "Send Data! Len = " << SendLen << endl;
+					break;
+				}
+			}
+		}
+	}
 
 	::WSACleanup();
 
